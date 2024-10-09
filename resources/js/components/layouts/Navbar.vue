fe6e2d99f9db910f01483e5175d97919cb488d1d
Md Zahid Hasan <jahid1234.jh@gmail.com>
2024-10-09 14:43:43 +0600

    first

---
+++ resources/js/components/layouts/Navbar.vue
@@ -0,0 +1,176 @@
+<template>
+    <div class="topbar">
+        <!-- LOGO -->
+        <div class="topbar-left">
+            <a href="index.html" class="logo">
+                <span style="color: white">LA MONITORING </span><i>LA MONITORING</i>
+            </a>
+        </div>
+        <nav class="navbar-custom">
+            <ul class="navbar-right list-inline float-right mb-0">
+                <!-- full screen -->
+                <li class="dropdown notification-list list-inline-item d-none d-md-inline-block">
+                    <a class="nav-link waves-effect" href="#" id="btn-fullscreen"><i class="mdi mdi-fullscreen noti-icon"></i></a>
+                </li>
+
+                <!-- notification -->
+                <!-- notification -->
+                <li class="dropdown notification-list list-inline-item">
+                    <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <i class="mdi mdi-bell-outline noti-icon"></i> <span class="badge badge-pill badge-danger noti-icon-badge">3</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg">
+                        <!-- item-->
+                        <h6 class="dropdown-item-text">Total Message {{notifications.length}}</h6>
+                        <div class="slimscroll notification-item-list" v-for="notification in notifications" :key="notification.id">
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-warning"><i class="mdi mdi-message-text-outline"></i></div>
+                                <p class="notify-details">  {{ notifications.message }}</p>
+                            </a>
+                        </div>
+                        <!-- All-->
+                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary">View all <i class="fi-arrow-right"></i></a>
+                    </div>
+                </li>
+                <li class="dropdown notification-list list-inline-item">
+                    <div class="dropdown notification-list nav-pro-img">
+                        <a class="dropdown-toggle nav-link arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                            <img src="assets/images/users/user-4.jpg" alt="user" class="rounded-circle">
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right profile-dropdown">
+                            <!-- item-->
+                            <button @click="openUserModel" class="dropdown-item" title="profile"> <i class="mdi mdi-account-circle m-r-5"></i>  Profile</button>
+
+                        </div>
+                    </div>
+                </li>
+
+                <li  class="notification-list list-inline-item">
+                    <div class="nav-link waves-effect" style="padding: 0">
+                        <p class="pad-cus-badge badge badge-primary">{{me.roles ? me.roles.RoleName:''}}</p>
+                    </div>
+
+                </li>
+                <li class="notification-list list-inline-item">
+                    <div class="nav-link waves-effect" style="padding: 0">
+                        <button @click="logout" title="Logout" type="button" class="btn"><i class="ti-power-off"></i></button>
+                    </div>
+                </li>
+            </ul>
+            <ul class="list-inline menu-left mb-0">
+                <li class="float-left">
+                    <button class="button-menu-mobile open-left waves-effect"><i class="mdi mdi-menu"></i></button>
+                </li>
+            </ul>
+
+        </nav>
+        <div v-if="myModel">
+            <transition name="model">
+                <div class="modal-mask">
+                    <div class="modal-wrapper">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title"> Update Profile</h4>
+                                    <button type="button"  @click="myModel=false" class="close"><span aria-hidden="true">&times;</span></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>User Id</label>
+                                                <input type="text" class="form-control"  v-model="me.UserID" readonly/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Email</label>
+                                                <input type="text" class="form-control" v-model="me.Email" readonly/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group" style="margin:0">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control" v-model="updatePassword"/>
+                                    </div>
+                                    <div class="form-group" style="margin:0">
+                                        <label> Confirm Password</label>
+                                        <input type="password" class="form-control" v-model="confirmUpdatePassword"/>
+                                    </div>
+                                    <br />
+                                    <div align="center">
+                                        <button class="btn btn-primary" @click="updateUserModel"> Update</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </transition>
+        </div>
+    </div>
+</template>
+<script >
+import {Common} from "../../mixins/common";
+import {baseurl} from "../../base_url";
+import {bus} from "../../app";
+
+export default {
+    mixins: [Common, baseurl],
+    data() {
+        return {
+            menus: [],
+            authUser: {},
+            myModel:false,
+            updatePassword:'',
+            confirmUpdatePassword: '',
+            notifications: []
+        }
+    },
+    mounted() {
+        this.listenForNotifications();
+    },
+    computed: {
+        me() {
+            return this.$store.state.me
+        }
+    },
+    methods: {
+        logout() {
+            this.axiosPost("logout", {}, (response) => {
+                    localStorage.setItem("token", "");
+                    this.$router.push(this.mainOrigin + "login");
+                    this.successNoti(response.message)
+                },
+                (error) => {
+                    this.errorNoti(error);
+                }
+            );
+        },
+        listenForNotifications() {
+            Echo.private('userCreate')
+                .notification((notification) => {
+                    this.notifications.push(notification);
+                });
+        },
+        openUserModel(){
+            this.myModel = true;
+        },
+        //Update User Password
+        updateUserModel(){
+            let url = 'user/password-change';
+            this.axiosPost(url, {
+                userId: this.me.UserID,
+                updatePassword: this.updatePassword,
+                confirmUpdatePassword: this.confirmUpdatePassword,
+            }, (response) => {
+                this.successNoti(response.message);
+                this.myModel = false;
+            }, (error) => {
+                this.errorNoti(error);
+            })
+        }
+    }
+}
+</script>
